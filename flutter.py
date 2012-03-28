7d9f6d0b5a8315597a524dc5655de711c8370022
i80and <i80and@gmail.com>
2012-03-28 09:42:19 -0500

    Improve comments, add some docstrings.

--- flutter.py
+++ flutter.py
@@ -5,16 +5,21 @@ debug = True
 
 
 class TypeSpecifier(metaclass=abc.ABCMeta):
+    """Base-class for special type containers."""
     @abc.abstractmethod
     def validate_type(self, x) -> bool:
+        """Verify that x satisfies this type specifier."""
         pass
 
     def wrap_type(self, x):
+        """Some types (like functions) cannot have their type resolved just
+           yet, and must be safety-wrapped for later analysis."""
         return x
 
 
 def has_members(a, klass) -> bool:
     """Returns whether or not a contains all of klass's properties."""
+    # Type containers have special validators
     if isinstance(klass, TypeSpecifier):
         return klass.validate_type(a)
 
@@ -24,6 +29,7 @@ def has_members(a, klass) -> bool:
 
 
 class Union(TypeSpecifier):
+    """Type container requiring that a value be one among a set of types."""
     def __init__(self, *args):
         self.possible_types = set(args)
 
@@ -36,6 +42,8 @@ class Union(TypeSpecifier):
 
 
 class UnboundedUniform(TypeSpecifier):
+    """Type container requiring that each element in a value be of a certain
+       type."""
     def __init__(self, arg):
         self.type = arg
 
@@ -48,6 +56,8 @@ class UnboundedUniform(TypeSpecifier):
 
 
 class Tuple(TypeSpecifier):
+    """Type container requiring that a value have N elements, each of a
+       predetermined type."""
     def __init__(self, *args):
         self.length = len(args)
         self.types = args
@@ -64,10 +74,16 @@ class Tuple(TypeSpecifier):
 
 
 def create_function_checker(prototype, f):
+    """Returns a wrapper for f demanding that f fits the given prototype."""
     def check_function(*args, **kwargs):
+        """If the arguments are right, call f() with them, and check its
+           return value."""
+        # Check the argument array arity
         if len(args) != (len(prototype) - 1):
             raise TypeError('{0}() has bad argument arity'.format(f.__name__))
 
+        # Ensure that the argument array is correct, wrapping functions as
+        # necessary before passing them on to f()
         wrapped_args = []
         for pair in zip(prototype, args):
             if not has_members(pair[1], pair[0]):
@@ -78,11 +94,10 @@ def create_function_checker(prototype, f):
             else:
                 wrapped_args.append(pair[1])
 
+        # Check f()'s return value, and wrap it if necessary before returning
         val = f(*wrapped_args, **kwargs)
-
         if not has_members(val, prototype[-1]):
             raise TypeError(val)
-
         if isinstance(prototype[-1], TypeSpecifier):
             val = prototype[-1].wrap_type(val)
 
@@ -92,6 +107,7 @@ def create_function_checker(prototype, f):
 
 
 class Function(TypeSpecifier):
+    """A type container representing a function."""
     def __init__(self, *args):
         self.prototype = args
 
@@ -102,12 +118,14 @@ class Function(TypeSpecifier):
         return True
 
     def wrap_type(self, f):
+        """Functions cannot have their type determined upon instantiation."""
         wrapper = create_function_checker(self.prototype, f)
         wrapper.__name__ = '{0} (flutter wrapped)'.format(f.__name__)
         return wrapper
 
 
 def check(*arg_types):
+    """Simple function decorator that checks the given function."""
     def inner(f):
         if not debug:
             return f
